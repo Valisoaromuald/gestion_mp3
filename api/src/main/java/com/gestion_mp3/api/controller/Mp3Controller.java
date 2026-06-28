a7098ce9e9762449591101b9b70a18f86518415c
Valisoaromuald <romualdrabenandrasana@gmail.com>
2026-06-28 18:51:21 +0300

    fix:moodification module mp3

--- api/src/main/java/com/gestion_mp3/api/controller/Mp3Controller.java
+++ api/src/main/java/com/gestion_mp3/api/controller/Mp3Controller.java
@@ -1,9 +1,21 @@
 // Mp3Controller.java
 package com.gestion_mp3.api.controller;
+
+import com.gestion_mp3.api.model.Album;
+import com.gestion_mp3.api.model.Artiste;
+import com.gestion_mp3.api.model.Genre;
 import com.gestion_mp3.api.model.Mp3;
 import com.gestion_mp3.api.service.Mp3Service;
+
+import java.io.IOException;
+
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/mp3")
@@ -13,8 +25,22 @@ public class Mp3Controller {
     public Mp3Controller(Mp3Service service) {
         this.service = service;
     }
-    @PostMapping
-    public ResponseEntity<Mp3> inserer(@RequestBody Mp3 mp3) {
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Mp3> inserer(
+            @RequestParam("fichier") MultipartFile fichier,
+            @RequestParam(value = "id_artiste", required = false) Integer idArtiste,
+            @RequestParam(value = "id_album", required = false) Integer idAlbum,
+            @RequestParam(value = "id_genre", required = false) Integer idGenre) throws IOException {
+        Mp3 mp3 = new Mp3();
+        mp3.setFichier(fichier.getBytes());
+        if (idArtiste != null)
+            mp3.setArtiste(new Artiste(idArtiste, null, null));
+        if (idAlbum != null)
+            mp3.setAlbum(new Album(idAlbum, null, null));
+        if (idGenre != null)
+            mp3.setGenre(new Genre(idGenre, null, null));
+
         return ResponseEntity.ok(service.inserer(mp3));
     }
 }
--- api/src/main/java/com/gestion_mp3/api/model/Mp3.java
+++ api/src/main/java/com/gestion_mp3/api/model/Mp3.java
@@ -1,11 +1,13 @@
 package com.gestion_mp3.api.model;
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
@@ -21,7 +23,9 @@ public class Mp3 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private Byte fichier;
+    @Lob
+    @Column(name = "fichier")
+    private byte[] fichier;
     @OneToOne(mappedBy = "mp3",cascade=CascadeType.ALL,fetch=FetchType.LAZY)
     private Metadata metadata;
     @ManyToOne
