0ed764450220f9a226000373993d6b82e5ff35cd
Valisoaromuald <romualdrabenandrasana@gmail.com>
2026-07-01 00:22:56 +0300

    insensiblite a la casse

--- api/src/main/java/com/gestion_mp3/api/repository/ArtisteRepository.java
+++ api/src/main/java/com/gestion_mp3/api/repository/ArtisteRepository.java
@@ -12,6 +12,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface ArtisteRepository extends JpaRepository<Artiste, Integer> {
-    @Query(value="SELECT * FROM artiste WHERE nom LIKE CONCAT('%', :nom, '%')",nativeQuery=true)
+    @Query(value="SELECT * FROM artiste WHERE  LOWER(nom) = LOWER(:nom)",nativeQuery=true)
     public Optional<Artiste>findByName(@Param("nom") String nom);
 }
