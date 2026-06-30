4a0d5848fd3d9ef4ff90554c22ebd0d62ae3ffb8
Valisoaromuald <romualdrabenandrasana@gmail.com>
2026-06-30 23:38:08 +0300

    ajout d'import oublie

--- api/src/main/java/com/gestion_mp3/api/model/Langue.java
+++ api/src/main/java/com/gestion_mp3/api/model/Langue.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
